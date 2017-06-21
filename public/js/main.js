dd641ebd071437a1c5fbddff13498e7e8b39bf03
Lijun Xue <xljroy@gmail.com>
2017-06-20 21:01:36 -0700

    We need to flyback

--- public/js/main.js
+++ public/js/main.js
@@ -3,20 +3,39 @@ $(document).ready(function(){
 	$("#roy-hover").hover(function(){
 		$("#roy-cover").fadeIn();
 	}, function(){
-		$("#roy-cover").fadeOut();
+		$("#roy-cover").fadeOut("fast");
 	});
 
 	$("#xenia-hover").hover(function(){
 		$("#xenia-cover").fadeIn();
 	}, function(){
-		$("#xenia-cover").fadeOut();
+		$("#xenia-cover").fadeOut("fast");
 	});
 
 	$("#roy-hover").click(function(){
-		$(".center-container").css('left', '100%');
+		tar = $(".center-container")
+		if (tar.hasClass('toggle')) {
+			tar.css('left', '50%');
+			tar.removeClass('toggle');
+			$("#roy-cover").fadeOut("fast");
+		} else {
+			tar.css('left', '100%');
+			tar.addClass('toggle');
+			$("#roy-cover").fadeOut("fast");
+		}
+		
 	});
 
 	$("#xenia-hover").click(function(){
-		$(".center-container").css('left', '0');
+		tar = $(".center-container")
+		if (tar.hasClass('toggle')) {
+			tar.css('left', '50%');
+			tar.removeClass('toggle');
+			$("#xenia-cover").fadeOut("fast");
+		} else {
+			tar.css('left', 0);
+			tar.addClass('toggle');
+			$("#xenia-cover").fadeOut("fast");
+		}
 	});
 })
