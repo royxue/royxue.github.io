11d5cea56b37b05eeafe2a86b90b821088bec7d4
Lijun Xue <xljroy@gmail.com>
2017-09-02 15:27:26 -0700

    Add hamburger btn

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import cx from 'classnames';
 import PropTypes from 'prop-types';
 import Link from 'gatsby-link';
 import Helmet from 'react-helmet';
+import { Layout, Menu }  from 'antd';
 
-import Header from '../components/header'
+import 'antd/dist/antd.css';
+import '../public/css/hamburgers.min.css'
+import '../public/css/illustraion.css';
+import '../public/css/index.css';
 
-import './index.css'
+const { Header, Content, Footer } = Layout;
 
-export default class Layout extends React.Component {
+export default class LayoutTemplate extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      openMenu: false,
+    }
+  }
+
+  toggleMenu = () => {
+    const { openMenu } = this.state;
+    this.setState({openMenu: !openMenu });
+  }
+
   render() {
     return (
       <div>
@@ -19,14 +36,19 @@ export default class Layout extends React.Component {
             name: 'keywords',
             content: 'love, life'
           }]}/>
-        <Header/>
-        <div style={{
-          width: '60%',
-          paddingTop: '4rem',
-          margin: '0 auto',
-        }}>
-          {this.props.children()}
-        </div>
+        <Layout style={{ minHeight: '100vh' }}>
+          <div className={cx("hamburger hamburger--spin hamburger-btn", {"is-active": this.state.openMenu})} onClick={this.toggleMenu}>
+            <span className="hamburger-box">
+              <span className="hamburger-inner"></span>
+            </span>
+          </div>
+          <Content>
+            {this.props.children()}
+          </Content>
+          <Footer className='footer flex jc-center'>
+            Roy and Xenia ©2018 Created with Love
+          </Footer>
+        </Layout>
       </div>
     )
   }
